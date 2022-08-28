3a45fa812b7ae59a1b5f4bb59ffc0e5831455246
akkarat <akkarat1775@gmail.com>
2022-08-28 23:15:39 +0700

    codelab6

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MTE3MTQxLCJleHAiOjE2NTkxMjA3NDEsIm5iZiI6MTY1OTExNzE0MSwianRpIjoiYjhNSHFSM0psRkU1T3JpcCIsInN1YiI6ODMsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.zzNktV9U3eVidZr6Yb5U5qLp4eNDh3K3G8VSvYv9nYc"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzAxMjA1LCJleHAiOjE2NjE3MDQ4MDUsIm5iZiI6MTY2MTcwMTIwNSwianRpIjoiS3g1elRjeVYyaGNsRWtoNyIsInN1YiI6ODMsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.dAcUSPH2-1xtV3VwtSoLjFqtFX3SZj6qD5hAf9WIpvI"; // put access token
 const student = {
   name: 'Akkarat Subsri', // replace with your full name.
   age: 19, // put your age.
